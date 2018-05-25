8a38e7fbb34a100e74e3b8edee572d4602c9dada
gijsoman <gijsbakker1@hotmail.com>
2018-05-25 15:36:18 +0200

    Player moves in the direction I want. It doesnt stop though. Need to fix.

--- Assets/Scripts/PlayerMotor.cs
+++ Assets/Scripts/PlayerMotor.cs
@@ -2,12 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
 public class PlayerMotor : MonoBehaviour
 {
     public float PlayerSpeed = 5;
 
+    private Rigidbody playerRigidBody;
+
+    private void Start()
+    {
+        playerRigidBody = GetComponent<Rigidbody>();
+    }
+
     public void MoveInDirection(Vector3 direction)
     {
-
+        playerRigidBody.AddForce(direction * PlayerSpeed);
     }
 }
