bea978654a293c804ba625a48f8cd93aec1854fa
gijsoman <gijsbakker1@hotmail.com>
2018-05-25 15:21:43 +0200

    Added playerMotor script that is going to actually move the player. Working on it.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,13 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(PlayerMotor))]
 public class PlayerController : MonoBehaviour
 {
-	private void Update ()
+    private PlayerMotor motor;
+
+    private void Start()
+    {
+        motor = GetComponent<PlayerMotor>();
+    }
+
+    private void Update ()
     {
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
 
+        Vector3 movingDirection = new Vector3(moveHorizontal, 0, moveVertical);
+
+        //Remove the log if working.
         Debug.Log(moveHorizontal + " | " + moveVertical);
+        motor.MoveInDirection(movingDirection);
 	}
 }
---
+++ Assets/Scripts/PlayerMotor.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMotor : MonoBehaviour
+{
+    public float PlayerSpeed = 5;
+
+    public void MoveInDirection(Vector3 direction)
+    {
+
+    }
+}
