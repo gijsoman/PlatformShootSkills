29ad1b64bb4ce2a2ca586dd65240c8f455e5d2af
gijsoman <gijsbakker1@hotmail.com>
2018-05-29 18:14:07 +0200

    I am currently using the unity third person controller to create my own.

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -4,14 +4,25 @@
 public class PlayerController : MonoBehaviour
 {
     private PlayerMotor motor;
+    private Vector3 move;
+    private bool jumping;
 
     private void Start()
     {
         motor = GetComponent<PlayerMotor>();
     }
 
+    private void Update()
+    {
+        if (!jumping)
+        {
+            jumping = Input.GetButtonDown("Jump");
+        }
+    }
+
     private void FixedUpdate()
     {
+        //read the inputs
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
