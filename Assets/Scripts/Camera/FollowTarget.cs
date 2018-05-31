79d0d1cff392b9da1cffb68706316d420ea708ce
gijsoman <gijsbakker1@hotmail.com>
2018-05-31 10:55:47 +0200

    Deleted the FollowTarge script from the standard assets because I wanted to use that name. Made my own followTarget script.

---
+++ Assets/Scripts/Camera/FollowTarget.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowTarget : MonoBehaviour
+{
+    [SerializeField] private Vector3 offset;
+    public Transform target;
+
+	void Update ()
+    {
+        transform.position = target.position + offset;
+	}
+}
