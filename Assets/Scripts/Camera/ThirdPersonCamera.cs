b7a920c89a2c085ee861ebd3dbf3c123c098c16b
gijsoman <gijsbakker1@hotmail.com>
2018-05-31 13:06:41 +0200

    We can now zoom in and out with a maximum and a minimum zoom :D.

--- Assets/Scripts/Camera/ThirdPersonCamera.cs
+++ Assets/Scripts/Camera/ThirdPersonCamera.cs
@@ -8,12 +8,18 @@ public class ThirdPersonCamera : MonoBehaviour
     [SerializeField] private bool invertAxis = false;
     [SerializeField] private float distanceFromTarget = 1f;
     [SerializeField] private Transform target;
+    [SerializeField] private float minZoom = 1;
+    [SerializeField] private float maxZoom = 10;
 
+    private float zoom = 10f;
     private float yaw;
     private float pitch;
 
     void Update()
     {
+        zoom -= Input.GetAxis("Mouse ScrollWheel");
+        zoom = Mathf.Clamp(zoom, minZoom, maxZoom);
+
         if (!invertAxis)
         {
             yaw += Input.GetAxis("Mouse X") * mouseSensitivity;
@@ -28,6 +34,6 @@ public class ThirdPersonCamera : MonoBehaviour
         Vector3 targetRotation = new Vector3(pitch, yaw);
         transform.eulerAngles = targetRotation;
 
-        transform.position = target.position - transform.forward * distanceFromTarget;
+        transform.position = target.position - transform.forward * distanceFromTarget * zoom;
 	}
 }
