0e9601885909304997ed06e75bba453d7f42198c
gijsoman <gijsbakker1@hotmail.com>
2018-05-31 12:34:59 +0200

    Making a third person camera. Using a tutorial to get the hang of it. Pretty hard imo

--- Assets/Scripts/Camera/FollowTarget.cs
+++ Assets/Scripts/Camera/FollowTarget.cs
@@ -5,10 +5,17 @@ using UnityEngine;
 public class FollowTarget : MonoBehaviour
 {
     [SerializeField] private Vector3 offset;
-    public Transform target;
+    public Transform Target;
 
 	void Update ()
     {
-        transform.position = target.position + offset;
+        transform.position = Target.position + offset;
+
+        
 	}
+
+    private void LateUpdate()
+    {
+        transform.RotateAround(Target.position, Vector3.up, Target.rotation.eulerAngles.y);
+    }
 }
---
+++ Assets/Scripts/Camera/ThirdPersonCamera.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonCamera : MonoBehaviour
+{
+    [SerializeField] private float mouseSensitivity = 10f;
+    [SerializeField] private bool invertAxis;
+    [SerializeField] private Vector3 offset;
+    [SerializeField] private Transform target;
+
+    private float yaw;
+    private float pitch;
+
+    void Update()
+    {
+        if (!invertAxis)
+        {
+            yaw += Input.GetAxis("Mouse X") * mouseSensitivity;
+            pitch -= Input.GetAxis("Mouse Y") * mouseSensitivity;
+        }
+        else
+        {
+            yaw -= Input.GetAxis("Mouse X") * mouseSensitivity;
+            pitch += Input.GetAxis("Mouse Y") * mouseSensitivity;
+        }
+
+        Vector3 targetRotation = new Vector3(pitch, yaw);
+        transform.eulerAngles = targetRotation;
+
+        transform.position = target.position + offset;
+	}
+}
