2bf3f7d0c3a29395b58b428ff13ef760cead8920
gijsoman <gijsbakker1@hotmail.com>
2018-06-01 14:25:54 +0200

    Creating a seperate script for the zoom function.

--- Assets/Scripts/Camera/ThirdPersonCamera.cs
+++ Assets/Scripts/Camera/ThirdPersonCamera.cs
@@ -6,8 +6,8 @@ public class ThirdPersonCamera : MonoBehaviour
     [SerializeField] private bool invertAxis = false;
     [SerializeField] private float distanceFromTarget = 1f;
     [SerializeField] private Transform target;
-    [SerializeField] private float minZoom = 1;
-    [SerializeField] private float maxZoom = 10;
+    //[SerializeField] private float minZoom = 1;
+    //[SerializeField] private float maxZoom = 10;
     [SerializeField] private float zoomSpeed = 1;
     [SerializeField] private float pitchMin = 40;
     [SerializeField] private float pitchMax = 85;
---
+++ Assets/ThirdPersonCameraZoom.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(ThirdPersonCamera))]
+public class ThirdPersonCameraZoom : MonoBehaviour
+{
+    [SerializeField] private float minimumZoom = 1f;
+    [SerializeField] private float maximumZoom = 10f;
+    [SerializeField] private float zoomSpeed = 1f;
+
+    private ThirdPersonCamera thirdPersonCamera;
+
+    private void Start()
+    {
+        thirdPersonCamera = GetComponent<ThirdPersonCamera>();
+    }
+
+}
