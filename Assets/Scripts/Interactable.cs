afb6ab81aee83bd7dc38ca8e71b3f9037c76da4d
gijsoman <gijsbakker1@hotmail.com>
2018-06-04 11:56:56 +0200

    Got a working interaction. Now im constatly checking the distance to the player. Why am i not using a trigger? making a trigger.

---
+++ Assets/Scripts/Interactable.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class Interactable : MonoBehaviour
+{
+    [SerializeField] private float radius = 3f;
+    [SerializeField] private Transform placeOfInteraction;
+    [SerializeField] private Transform player;
+
+    private bool hasInteracted = false;
+
+    private void Interact()
+    {
+        if (!hasInteracted)
+        {
+            Debug.Log("Interacting");
+        }
+    }
+
+    private void Update()
+    {
+        float distance = Vector3.Distance(placeOfInteraction.position, player.position);
+        if (distance <= radius)
+        {
+            Interact();
+            hasInteracted = true;
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(placeOfInteraction.position, radius);
+    }
+}
