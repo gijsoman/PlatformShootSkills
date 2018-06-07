237867620f5a3f8bfba9d9ca56da9c92f5102938
gijsoman <gijsbakker1@hotmail.com>
2018-06-07 14:53:56 +0200

    adding different functionalities to slot so we dont have to get the rect component of slot.

--- Assets/Scripts/Inventory & Items/Slot.cs
+++ Assets/Scripts/Inventory & Items/Slot.cs
@@ -1,7 +1,9 @@
-﻿using UnityEngine;
+﻿using UnityEngine.UI;
+using UnityEngine;
 
 [RequireComponent(typeof(RectTransform))]
 [RequireComponent(typeof(CanvasRenderer))]
+[RequireComponent(typeof(Image))]
 public class Slot : MonoBehaviour
 {
     [SerializeField] private Vector2Int widthHeight = new Vector2Int(10, 10);
@@ -9,20 +11,23 @@ public class Slot : MonoBehaviour
     private bool isOccupied;
     private RectTransform rect;
 
-    public Slot(int _width, int _height)
-    {
-        widthHeight.x = _width;
-        widthHeight.y = _height;
-    }
-
-    private void Start()
+    private void Awake()
     {
         rect = GetComponent<RectTransform>();
-        SetWidthAndHeight(widthHeight.x, widthHeight.y);
     }
 
     public void SetWidthAndHeight(int _width, int _height)
     {
         rect.sizeDelta = new Vector2Int(_width, _height);
+    }
+
+    public void SetAnchoredPosition(int _xPosition, int _yPosition)
+    {
+        rect.anchoredPosition = new Vector2(_xPosition, _yPosition);
+    }
+
+    public void SetAnchor(Vector2 _minXY, Vector2 _maxXY, Vector2 _pivot)
+    {
+        
     }
 }
--- Assets/Scripts/Inventory & Items/SlotGrid.cs
+++ Assets/Scripts/Inventory & Items/SlotGrid.cs
@@ -6,6 +6,9 @@ public class SlotGrid : MonoBehaviour
 {
     [SerializeField] private Vector2Int amountOfSlots = new Vector2Int(10, 10);
     [SerializeField] private Vector2Int slotSizes = new Vector2Int(10, 10);
+    [SerializeField] private int slotPadding = 10;
+
+    private int gridStartPosition = 0;
 
     //Using a nested list instead of an array in case we want to increase the size of the inventory.
     public List<List<GameObject>> slots = new List<List<GameObject>>();
@@ -20,16 +23,19 @@ public class SlotGrid : MonoBehaviour
         for (int y = 0; y < amountOfSlots.y; y++)
         {
             List<GameObject> slotsSubList = new List<GameObject>();
-            for (int X = 0; X < amountOfSlots.x; X++)
+            for (int x = 0; x < amountOfSlots.x; x++)
             {
                 GameObject slot = new GameObject();
                 slot.transform.SetParent(transform);
-                slot.AddComponent<Slot>();
+                Slot currentSlot = slot.AddComponent<Slot>();
+                currentSlot.SetWidthAndHeight(slotSizes.x, slotSizes.y);
+                currentSlot.SetAnchoredPosition(gridStartPosition + slotSizes.x * x, gridStartPosition + y * slotSizes.y);
                 slotsSubList.Add(slot);
             }
             slots.Add(slotsSubList);
         }
     }
+
 }
 
 
