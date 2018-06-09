26de7c061015f9e2ffc1bb691cd147e5c27849e4
gijsoman <gijsbakker1@hotmail.com>
2018-06-09 15:46:30 +0200

    Getting closer. Width of the cells is now determined on the width of the gridcontainer.

--- Assets/Scripts/Inventory & Items/SlotGrid.cs
+++ Assets/Scripts/Inventory & Items/SlotGrid.cs
@@ -12,12 +12,14 @@ public class SlotGrid : MonoBehaviour
     //[SerializeField] private int slotPadding = 10;
 
     private float gridStartPosition = 0;
+    private float totalslotHeight;
 
     RectTransform rect;
 
     private void Start()
     {
         rect = GetComponent<RectTransform>();
+        Debug.Log(rect.sizeDelta.x);
         CreateSlots();
     }
 
@@ -28,20 +30,23 @@ public class SlotGrid : MonoBehaviour
             List<GameObject> slotsSubList = new List<GameObject>();
             for (int x = 0; x < amountOfSlots.x; x++)
             {
-                GameObject slot = new GameObject("Slot " + x + ", " + y);
+                GameObject slot = new GameObject("Slot " + x + ", ");
                 slot.transform.SetParent(transform);
                 Slot currentSlot = slot.AddComponent<Slot>();
                 //set slotWidthAndHeight based on the width of our transform.
                 slotWidthAndHeight = rect.sizeDelta.x / amountOfSlots.x;
+                currentSlot.transform.localScale = new Vector3(1,1,1);
                 currentSlot.SetWidthAndHeight(slotWidthAndHeight, slotWidthAndHeight);
-                currentSlot.SetAnchor(new Vector2(0, 0), new Vector2(1, 1), new Vector2(0, 1)); //fix this with an enumerator so the user can choose the place of the anchor and of the pivot. or maybe dont let the user choose and always create the grid within the parent container.
-                //currentSlot.SetAnchoredPosition(gridStartPosition + slotWidthAndHeight * x, gridStartPosition - y * slotWidthAndHeight);
+                currentSlot.SetAnchor(new Vector2(0, 1), new Vector2(0, 1), new Vector2(0, 1)); //fix this with an enumerator so the user can choose the place of the anchor and of the pivot. or maybe dont let the user choose and always create the grid within the parent container.
+                currentSlot.SetAnchoredPosition(gridStartPosition + slotWidthAndHeight * x, gridStartPosition - y * slotWidthAndHeight);
                 slotsSubList.Add(slot);
+                totalslotHeight += slotWidthAndHeight;
             }
             Slots.Add(slotsSubList);
+
+            rect.sizeDelta = new Vector2(rect.sizeDelta.x, totalslotHeight);
         }
     }
 
 }
-
 
