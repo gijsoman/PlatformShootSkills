a133dfccf90ea478250fc50247f76f4414435ce3
gijsoman <gijsbakker1@hotmail.com>
2018-06-09 16:14:39 +0200

    Okay so we can make a grid. We even show the size of the grid by resizing the grid container in the onvalidate function based on the amount of slots. Now i want to add spacing to the cells.

--- Assets/Scripts/Inventory & Items/SlotGrid.cs
+++ Assets/Scripts/Inventory & Items/SlotGrid.cs
@@ -9,18 +9,31 @@ public class SlotGrid : MonoBehaviour
     [SerializeField] private Vector2Int amountOfSlots = new Vector2Int(10, 10);
 
     private float slotWidthAndHeight;
+    //ADD SLOT PADDING NOW!
     //[SerializeField] private int slotPadding = 10;
 
     private float gridStartPosition = 0;
-    private float totalslotHeight;
+    private float totalSlotHeight;
 
     RectTransform rect;
 
     private void Start()
     {
-        rect = GetComponent<RectTransform>();
         Debug.Log(rect.sizeDelta.x);
         CreateSlots();
+    }
+
+    private void OnValidate()
+    {
+        rect = GetComponent<RectTransform>();
+        Debug.Log("slotWidthAndHeight: " + slotWidthAndHeight);
+        Debug.Log("amountOfSlots X: " + amountOfSlots.x);
+        if (amountOfSlots.x != 0)
+        {
+            slotWidthAndHeight = rect.sizeDelta.x / amountOfSlots.x;
+        }
+        totalSlotHeight = amountOfSlots.y * slotWidthAndHeight;
+        rect.sizeDelta = new Vector2(rect.sizeDelta.x, totalSlotHeight);
     }
 
     private void CreateSlots()
@@ -30,23 +43,18 @@ public class SlotGrid : MonoBehaviour
             List<GameObject> slotsSubList = new List<GameObject>();
             for (int x = 0; x < amountOfSlots.x; x++)
             {
-                GameObject slot = new GameObject("Slot " + x + ", ");
+                GameObject slot = new GameObject("Slot " + x + ", " + y);
                 slot.transform.SetParent(transform);
                 Slot currentSlot = slot.AddComponent<Slot>();
                 //set slotWidthAndHeight based on the width of our transform.
-                slotWidthAndHeight = rect.sizeDelta.x / amountOfSlots.x;
                 currentSlot.transform.localScale = new Vector3(1,1,1);
                 currentSlot.SetWidthAndHeight(slotWidthAndHeight, slotWidthAndHeight);
                 currentSlot.SetAnchor(new Vector2(0, 1), new Vector2(0, 1), new Vector2(0, 1)); //fix this with an enumerator so the user can choose the place of the anchor and of the pivot. or maybe dont let the user choose and always create the grid within the parent container.
                 currentSlot.SetAnchoredPosition(gridStartPosition + slotWidthAndHeight * x, gridStartPosition - y * slotWidthAndHeight);
                 slotsSubList.Add(slot);
-                totalslotHeight += slotWidthAndHeight;
             }
             Slots.Add(slotsSubList);
-
-            rect.sizeDelta = new Vector2(rect.sizeDelta.x, totalslotHeight);
         }
     }
-
 }
 
