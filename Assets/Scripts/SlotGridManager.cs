c2cb63e52722edf0aa77bb80fd7254612ae87701
gijsoman <gijsbakker1@hotmail.com>
2018-06-14 18:47:59 +0200

    No clue what to do now. I don't really understand the example anymore.

--- Assets/Scripts/SlotGridManager.cs
+++ Assets/Scripts/SlotGridManager.cs
@@ -3,6 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class SlotGridManager : MonoBehaviour
-{   
-
+{
+    private void StoreItem(GameObject _item)
+    {
+        Slot slot;
+        SlotGrid slotGrid;
+        Vector2Int itemSize = _item.GetComponent<Item>().AmountOfSlotsOccupying;
+        for (int y = 0; y < itemSize.y; y++)
+        {
+            for (int x = 0; x < itemSize.x; x++)
+            {
+                slot = slotgri
+            }
+        }
+    }
 }
