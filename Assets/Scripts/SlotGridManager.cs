a347bb10f1488af0520034437bf52bd2607aced4
gijsoman <gijsbakker1@hotmail.com>
2018-06-14 19:02:36 +0200

    I dont know what to do. Not understanding it. Trying again tommorrow.

--- Assets/Scripts/SlotGridManager.cs
+++ Assets/Scripts/SlotGridManager.cs
@@ -6,15 +6,5 @@ public class SlotGridManager : MonoBehaviour
 {
     private void StoreItem(GameObject _item)
     {
-        Slot slot;
-        SlotGrid slotGrid;
-        Vector2Int itemSize = _item.GetComponent<Item>().AmountOfSlotsOccupying;
-        for (int y = 0; y < itemSize.y; y++)
-        {
-            for (int x = 0; x < itemSize.x; x++)
-            {
-                slot = slotgri
-            }
-        }
     }
 }
